c019d3cad2d43479d49fa7f87a18c31fc79b82d7
Nicholas <wenann.howe@gmail.com>
2021-05-20 22:54:48 +0800

    completed model script and data loader

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -2,7 +2,6 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 
 # Create a pytorch dataset object
-
 class CreateDataset(Dataset):
 
     '''
@@ -12,11 +11,12 @@ class CreateDataset(Dataset):
         The __getitem__ function loads and returns a sample from the dataset at the given index idx. Returns a python dict
     '''
 
-    def __init__(self, text, targets, tokenizer, max_len):
+    def __init__(self, text, targets, tokenizer, max_len, chunksize):
         self.text = text
         self.targets = targets
         self.tokenizer = tokenizer
         self.max_len = max_len
+        self.chunksize = chunksize
 
     def __len__(self):
         return len(self.text)
@@ -28,33 +28,88 @@ class CreateDataset(Dataset):
 
         encoding = self.tokenizer.encode_plus(
             text,
-            add_special_tokens = True,
+            add_special_tokens = False,
             max_length = self.max_len,
             return_token_type_ids = False,
-            padding = 'max_length',
+            padding = False,
             truncation = True,
             return_attention_mask = True,
             return_tensors = 'pt')
 
+        input_id_chunks = list(encoding['input_ids'][0].split(self.chunksize - 2))
+        mask_chunks = list(encoding['attention_mask'][0].split(self.chunksize - 2))
+
+        # loop through each chunk
+        for i in range(len(input_id_chunks)):
+            # add CLS and SEP tokens to input IDs
+            input_id_chunks[i] = torch.cat([
+                torch.tensor([101]), input_id_chunks[i], torch.tensor([102])
+            ])
+            # add attention tokens to attention mask
+            mask_chunks[i] = torch.cat([
+                torch.tensor([1]), mask_chunks[i], torch.tensor([1])
+            ])
+            # get required padding length
+            pad_len = self.chunksize - input_id_chunks[i].shape[0]
+            # check if tensor length satisfies required chunk size
+            if pad_len > 0:
+                # if padding length is more than 0, we must add padding
+                input_id_chunks[i] = torch.cat([
+                    input_id_chunks[i], torch.Tensor([0] * pad_len)
+                ])
+                mask_chunks[i] = torch.cat([
+                    mask_chunks[i], torch.Tensor([0] * pad_len)
+                ])
+        
+        # Stack output
+        input_ids = torch.stack(input_id_chunks)
+        attention_mask = torch.stack(mask_chunks)
+
         out_dict = {
             'text': text,
-            'input_ids': encoding['input_ids'].flatten(),
-            'attention_mask': encoding['attention_mask'].flatten(),
-            'target': torch.tensor(target, dtype=torch.long)
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'n_chunks': torch.tensor([len(input_id_chunks)], dtype=torch.long),
+            'target': torch.tensor([target], dtype=torch.long)
         }
 
         return out_dict
 
 
+def custom_collate_fn(batch):
+    # return batches
+    batched = {}
+
+    for item in batch:
+        for key, value in item.items():
+            if key not in batched:
+                batched[key] = []
+            batched[key].append(value)
+
+    # concat list of tensors (if all elements in the list are tensors)
+    for key in batched.keys():
+        if all(isinstance(item, torch.Tensor) for item in batched[key]):
+            batched[key] = torch.cat(batched[key])
+
+    return batched
+
+
 # Create dataloader
-def create_data_loader(df, tokenizer, max_len, batch_size, sampler = None, shuffle = False, drop_last = False):
+def create_data_loader(df, tokenizer, max_len, batch_size, chunksize = 512, sampler = None, shuffle = False, drop_last = False):
 
     # create dataset object
     ds = CreateDataset(
         text = df['content'].to_numpy(), 
-        target = df['target'].to_numpy(), 
+        targets = df['target'].to_numpy(), 
         tokenizer = tokenizer, 
-        max_len = max_len
+        max_len = max_len,
+        chunksize = chunksize
         )
 
-    return DataLoader(dataset=ds, batch_size=batch_size, sampler=sampler, shuffle=shuffle, drop_last=drop_last)
+    return DataLoader(
+        dataset=ds, 
+        batch_size=batch_size, 
+        sampler=sampler, 
+        shuffle=shuffle, 
+        drop_last=drop_last, 
+        collate_fn=custom_collate_fn)
---
+++ src/models/model.py
@@ -0,0 +1,83 @@
+import torch
+from torch import nn, optim
+
+from src.models.attention import Attention
+
+# Transformers
+from transformers import BertTokenizerFast, BertModel, AdamW, get_linear_schedule_with_warmup
+
+class LinearBlock(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
+        super(LinearBlock, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.fc = nn.Linear(self.in_dim, self.out_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.fc(x)
+        out = self.relu(out)
+        return out
+
+
+class HIBERT(nn.Module):
+
+    def __init__(self, 
+                    PRE_TRAINED_MODEL_NAME, 
+                    n_classes, 
+                    add_linear=None, 
+                    attn_bias=False, 
+                    freeze_layer_count=-1):
+
+        super(HIBERT, self).__init__()
+
+        self.n_classes = n_classes
+        self.add_linear = add_linear
+        self.attn_bias = attn_bias
+        self.freeze_layer_count = freeze_layer_count
+
+        # Define model objects
+        self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME, return_dict=False, add_pooling_layer=False)
+        self.fc_in_size = self.bert.config.hidden_size
+
+        # Attention pooling layer
+        self.attention = Attention(dim=self.bert.config.hidden_size, attn_bias=self.attn_bias)
+
+        # fully connected layers
+        if self.add_linear is None:
+            self.fc = nn.ModuleList([nn.Linear(self.fc_in_size, self.n_classes)])
+        
+        else: 
+            self.fc_layers = [self.fc_in_size] + self.add_linear
+            self.fc = nn.ModuleList([
+                LinearBlock(self.fc_layers[i], self.fc_layers[i+1])
+                for i in range(len(self.fc_layers) - 1)
+                ])
+            
+            # no relu after last dense (cannot use LinearBlock)
+            self.fc.append(nn.Linear(self.fc_layers[-1], self.n_classes))
+
+    def forward(self, input_ids, attention_mask, n_chunks):
+
+        # Bert transformer (take sequential output)
+        output, _ = self.bert(
+            input_ids = input_ids, 
+            attention_mask = attention_mask
+            )
+
+        # group chunks together
+        output = output.split_with_sizes(n_chunks)
+
+        # loop through attention layer (need a loop as there are different sized chunks)
+        output = torch.stack([
+            self.attention(chunk.view(1, -1, self.bert.config.hidden_size)) 
+            for chunk in output
+            ])
+
+        # fully connected layers
+        for fc in self.fc:
+            output = fc(output)
+
+        return output
